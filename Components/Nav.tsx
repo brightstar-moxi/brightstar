55d73f3e4955b533ba0bc3afbcbb34516f96e04e
unknown <brightstarmoxiz2@gmail.com>
2024-12-05 05:20:13 +0100

    good morning

---
+++ Components/Nav.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md'>
+     <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
+        <h1 className=''>
+            BRIGHTSTAR
+            <span>TECH</span>
+        </h1>
+        <div>HOME</div>
+        <div>SERVICES</div>
+        <div>ABOUT</div>
+        <div>PROJECT</div>
+        <div>BLOG</div>
+        <div>CONTACT</div>
+        </div>
+        </div>
+  )
+}
+
+export default Nav
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+import Nav from '@/Components/Nav'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='text-9xl text-red-900'>HomePage</div>
+    <div className='overflow-x-hidden'>
+      <div>
+        <Nav/>
+      </div>
+    </div>
   )
 }
 
