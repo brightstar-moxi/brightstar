8545a412b4ac05502aeefac567ee040a3c5b4af6
unknown <brightstarmoxiz2@gmail.com>
2024-12-11 17:59:51 +0100

    good Evening

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -5,8 +5,8 @@ const Nav = () => {
     <div className='w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md'>
      <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
         <h1 className='flex-[0.6] cursor-pointer text-[25px] text-white font-bold'>
-            BRIGHTSTAR
-            <span className='text-yellow-300'>TECH</span>
+            BRIGHT
+            <span className='text-yellow-300'>STAR</span>
         </h1>
         <div className='nav-link'>HOME</div>
         <div className='nav-link'>SERVICES</div>
