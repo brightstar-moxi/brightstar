de4c4092386b6bae3a4dc46b316a053edec2ab22
unknown <brightstarmoxiz2@gmail.com>
2024-12-12 17:50:44 +0100

    good evening

--- Components/About.tsx
+++ Components/About.tsx
@@ -6,6 +6,9 @@ const About = () => {
         <div className='grid grid-cols-1 md:grid-cols-2 w-[80%] mx-auto gap-[3rem] items-center'>
             <div>
                 <h1 className='text-[20px] font-bold uppercase text-[#55a6a5] md-[1rem]'>ABOUT ME</h1>
+           <h2 className='text-[25px] md:text-[35px] lg:text-[45px] md:leading-[2rem] capitalize mb-[3rem] font-bold text-white'>
+            Transforming <span className='text-yellow-400'>Visions</span>
+             </h2>
             </div>
         </div>
     </div>
