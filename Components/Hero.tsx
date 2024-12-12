43d8a09df914dd81b3389237ec28375a3846530f
unknown <brightstarmoxiz2@gmail.com>
2024-12-12 16:38:02 +0100

    good afternoon

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Particle from './Particle'
 import TextEffect from './TextEffect'
 import Image from 'next/image'
-import { ArrowDownTrayIcon } from '@heroicons/react/20/solid'
+import { ArrowDownTrayIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 
 const Hero = () => {
     return (
@@ -14,16 +14,21 @@ const Hero = () => {
                         HI, I'M <span className='text-yellow-400'>MOSES</span>
                     </h1>
                     <TextEffect />
-                    <p className='mt-[2rem] text-[20px] text-[#ffffff92]'>
+                    <p className='mt-[1.5rem] text-[18px] text-[#ffffff92]'>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam maxime molestiae,
                         mollitia quod explicabo omnis architecto modi excepturi debitis consectetur suscipit,
                         ducimus doloremque totam iure eligendi quae. Tempora, qui cumque!
                     </p>
-                    <div className='mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:*:flex-row items-center sm:space-x-6'>
+                    <div className='mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6'>
                         <button className='px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold 
                         uppercase bg-[#55a6a5] text-black flex items-center space-x-2'>
                            <p>Download Cv</p> 
                            <ArrowDownTrayIcon className='w-[1.6rem] h-[1.7rem] text-black'/>
+                        </button>
+                        <button className='flex items-center space-x-2'>
+                            <PlayCircleIcon className='w-[4rem] h-[4rem] hover:text-yellow-400 transition-all duration-200
+                            text-[#55a6a5]'/>
+                            <p className='text-[20px] font-semibold text-white'>Watch The Video</p>
                         </button>
                     </div>
                 </div>
