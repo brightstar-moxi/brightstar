00b4c8159223d337a2668f68d3d4b67f673a1882
unknown <brightstarmoxiz2@gmail.com>
2024-12-12 15:34:49 +0100

    good afternoon

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import Particle from './Particle'
 import TextEffect from './TextEffect'
+import Image from 'next/image'
 
 const Hero = () => {
     return (
         <div className="h-[88vh]  bg-cover bg-center ">
             <Particle />
             <div className='w-[80%] sticky grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
-
+<div>
                 <h1 className='text-[35px] md:text-[50px] text-white font-bold'>
                     HI, I'M <span className='text-yellow-400'>MOSES</span>
                     </h1>
                     <TextEffect/>
+                    </div>
+                    <div className='w-[500px] hidden bg-[#55a6a5] relative lg:flex items-center rounded-full h-[500px]'>
+                <Image src="/images/me.jpg" alt="user" layout='fill' className='object-cover rounded-full'/>
             </div>
+            </div>
+          
         </div>
     )
 }
