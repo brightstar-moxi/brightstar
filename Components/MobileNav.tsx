73a1743002b2e738ca1edfecac2ac21716fafee6
unknown <brightstarmoxiz2@gmail.com>
2024-12-12 11:08:44 +0100

    good morning

---
+++ Components/MobileNav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MobileNav = () => {
+  return (
+    <div className={`fixed transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[10000] bg-[#0910la]`}>
+        <div className=''></div>
+        </div>
+  )
+}
+
+export default MobileNav
