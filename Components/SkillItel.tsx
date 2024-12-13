944f93e57ba6815021af955391bbeab992097e3e
unknown <brightstarmoxiz2@gmail.com>
2024-12-13 13:34:58 +0100

    good afternoon

--- Components/SkillItel.tsx
+++ Components/SkillItel.tsx
@@ -7,7 +7,17 @@ interface Props {
 
 const SkillItel = ({title,year}:Props) => {
   return (
-    <div className='mb-[4rem] md:mb-[8rem]'>SkillItel</div>
+    <div className='mb-[4rem] md:mb-[8rem]'>
+        <span className='px-[2rem] text-[#55a6a5] py-[0.9rem] font-bold text-[18px] border-[2px] border-[#55a6a5]'>
+            {year}
+            </span>
+            <h1 className='mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white'>
+                {title}
+            </h1>
+            <p className='text-[#aaaaaa] font-normal w-[80%] text-[17px] opacity-80'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum nobis impedit reiciendis obcaecati consectetur eveniet corporis dolores quos incidunt doloremque sunt quidem quod placeat, ducimus veniam quibusdam, aliquam mollitia laudantium?
+            </p>
+    </div>
   )
 }
 
