ccf99b11d1da700dbba1273a92607b776ff796cd
unknown <brightstarmoxiz2@gmail.com>
2024-12-13 11:29:15 +0100

    good mornning

--- Components/Service.tsx
+++ Components/Service.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Service = () => {
   return (
-    <div className='bg-[#121212] pt-[4rem] md:pt-[8rem] pb-[5rem]'></div>
+    <div className='bg-[#121212] pt-[4rem] md:pt-[8rem] pb-[5rem]'>
+        <p className='heading'>My <span className='text-yellow-400'>Service</span></p>
+    </div>
   )
 }
 
