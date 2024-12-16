f72bc6d1d8edbab96fe386a5ae81538262ba6dd7
unknown <brightstarmoxiz2@gmail.com>
2024-12-16 16:05:27 +0100

    my blog updating

--- Components/Blog.tsx
+++ Components/Blog.tsx
@@ -20,13 +20,13 @@ const Blog = () => {
                         />
                     </div>
                     <div className='w-[90%] text-center mx-auto bg-[#09101a] relative p-[2rem] mt-[-1rem]'>
-                        <div className='w-fit px-[2rem] py-[1rem] bg-[#55a6a5] relative mt-[-3rem] text-black font-semibold'>
+                        <div className='w-fit px-[2rem] py-[1rem] bg-[#55a6a5] relative mt-[-3rem] text-black font-semibold text-[17px] mx-auto'>
                             December 10, 2023
                         </div>
                         <div className='flex flex-col md:flex-row items-center mt-[1rem] space-x-4'>
                             <div className='flex items-center space-x-3'>
                                 <UserCircleIcon className='w-[1.5rem] mx-auto h-[1.5rem] text-[#55a6a5]'/>
-                                <p className='text-white'>By Brighstar</p>
+                                <p className='text-white'>By Brightstar</p>
                             </div>
                             <div className='flex items-center space-x-3'>
                                 <ChatBubbleLeftRightIcon className='w-[1.5rem] mx-auto h-[1.5rem] text-[#55a6a5]'/>
@@ -36,8 +36,58 @@ const Blog = () => {
                         <p className='mt-[1rem] text-white text-[18px] font-semibold'>Next js Fullstack Development</p>
                     </div>
                 </div>
-                <div></div>
-                <div></div>
+                <div>
+                    <div className='w-[100%] relative h-[400px]'>
+                        <Image
+                            src="/images/me.jpg"
+                            alt='blog'
+                            layout="fill"
+                            className='object-cover'
+                        />
+                    </div>
+                    <div className='w-[90%] text-center mx-auto bg-[#09101a] relative p-[2rem] mt-[-1rem]'>
+                        <div className='w-fit px-[2rem] py-[1rem] bg-[#55a6a5] relative mt-[-3rem] text-black font-semibold text-[17px] mx-auto'>
+                            December 13, 2023
+                        </div>
+                        <div className='flex flex-col md:flex-row items-center mt-[1rem] space-x-4'>
+                            <div className='flex items-center space-x-3'>
+                                <UserCircleIcon className='w-[1.5rem] mx-auto h-[1.5rem] text-[#55a6a5]'/>
+                                <p className='text-white'>By Patrick boy</p>
+                            </div>
+                            <div className='flex items-center space-x-3'>
+                                <ChatBubbleLeftRightIcon className='w-[1.5rem] mx-auto h-[1.5rem] text-[#55a6a5]'/>
+                                <p className='text-white'>Comments(9)</p>
+                            </div>
+                        </div>
+                        <p className='mt-[1rem] text-white text-[18px] font-semibold'>React New features</p>
+                    </div>
+                </div>
+                <div>
+                    <div className='w-[100%] relative h-[400px]'>
+                        <Image
+                            src="/images/me.jpg"
+                            alt='blog'
+                            layout="fill"
+                            className='object-cover'
+                        />
+                    </div>
+                    <div className='w-[90%] text-center mx-auto bg-[#09101a] relative p-[2rem] mt-[-1rem]'>
+                        <div className='w-fit px-[2rem] py-[1rem] bg-[#55a6a5] relative mt-[-3rem] text-black font-semibold text-[17px] mx-auto'>
+                            December 29, 2023
+                        </div>
+                        <div className='flex flex-col md:flex-row items-center mt-[1rem] space-x-4'>
+                            <div className='flex items-center space-x-3'>
+                                <UserCircleIcon className='w-[1.5rem] mx-auto h-[1.5rem] text-[#55a6a5]'/>
+                                <p className='text-white'>By Goodluck</p>
+                            </div>
+                            <div className='flex items-center space-x-3'>
+                                <ChatBubbleLeftRightIcon className='w-[1.5rem] mx-auto h-[1.5rem] text-[#55a6a5]'/>
+                                <p className='text-white'>Comments(7)</p>
+                            </div>
+                        </div>
+                        <p className='mt-[1rem] text-white text-[18px] font-semibold'>Next js New Features</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
