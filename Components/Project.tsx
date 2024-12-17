23efe43d9e088a2fd86770f6c5fab5751655b272
unknown <brightstarmoxiz2@gmail.com>
2024-12-17 16:20:08 +0100

    aos animation

--- Components/Project.tsx
+++ Components/Project.tsx
@@ -6,7 +6,7 @@ const Project = () => {
         <div className='bg-[#02050a] pt-[4rem] md:pt-[8rem] pb-[1rem]'>
             <h1 className='heading'>Pro<span className='text-yellow-400'>ject</span></h1>
             <div className='w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem]'>
-                <div>
+                <div data-aos="fade-up">
                     <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
                         <Image
                             src="/images/me.jpg"
@@ -16,7 +16,7 @@ const Project = () => {
                         />
                     </div>
                 </div>
-                <div>
+                <div data-aos="fade-up" data-aos-delay="300">
                     <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
                         <Image
                             src="/images/me.jpg"
@@ -26,7 +26,7 @@ const Project = () => {
                         />
                     </div>
                 </div>
-                <div>
+                <div data-aos="fade-up" data-aos-delay="600">
                     <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
                         <Image
                             src="/images/me.jpg"
@@ -36,7 +36,7 @@ const Project = () => {
                         />
                     </div>
                 </div>
-                <div>
+                <div data-aos="fade-up" data-aos-delay="900">
                     <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
                         <Image
                             src="/images/me.jpg"
@@ -46,7 +46,7 @@ const Project = () => {
                         />
                     </div>
                 </div>
-                <div>
+                <div data-aos="fade-up" data-aos-delay="1200">
                     <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
                         <Image
                             src="/images/me.jpg"
@@ -56,7 +56,7 @@ const Project = () => {
                         />
                     </div>
                 </div>
-                <div>
+                <div data-aos="fade-up" data-aos-delay="1600">
                     <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]'>
                         <Image
                             src="/images/me.jpg"
--- Components/Service.tsx
+++ Components/Service.tsx
@@ -6,7 +6,7 @@ const Service = () => {
         <div className='bg-[#121212] pt-[4rem] md:pt-[8rem] pb-[5rem]'>
             <p className='heading'>My <span className='text-yellow-400'>Service</span></p>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto items-center gap-[3rem] mt-[4rem] text-white'>
-                <div>
+                <div data-aos="fade-right">
                     <div className='bg-red-700 hover:scale-110 transition-all duration-300 hover:rotate-6 uppercase font-semibold text-center p-[2rem]'>
                         <CodeBracketSquareIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3faa8]' />
                         <h1 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]'>Frontend</h1>
@@ -16,7 +16,7 @@ const Service = () => {
                         </p>
                     </div>
                 </div>
-                <div>
+                <div data-aos="zoom-in" data-aos-delay="300">
                     <div className='bg-orange-700 hover:scale-110 transition-all duration-300  uppercase font-semibold text-center p-[2rem]'>
                         <RocketLaunchIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3faa8]' />
                         <h1 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]'>Backend</h1>
@@ -26,7 +26,7 @@ const Service = () => {
                         </p>
                     </div>
                 </div>
-                <div>
+                <div data-aos="fade-left" data-aos-delay="500">
                     <div className='bg-blue-700 hover:scale-110 transition-all duration-300 hover:rotate-6 uppercase font-semibold text-center p-[2rem]'>
                         <CommandLineIcon className='w-[6rem] h-[6rem] mx-auto text-[#d3faa8]' />
                         <h1 className='text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]'>Fullstack</h1>
